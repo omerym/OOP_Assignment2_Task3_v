862d1b0f54ef8930d5cafb3c0f98e7b43ed8221c
Omer Yassir <omerym10@gmail.com>
2023-11-13 23:15:13 +0200

    Menu

--- OOP_Assignment2_Task3_v/OOP_Assignment2_Task3_v.cpp
+++ OOP_Assignment2_Task3_v/OOP_Assignment2_Task3_v.cpp
@@ -11,21 +11,53 @@ using namespace std;
 
 int main()
 {
-	char c[] = "C:\\Users\\Omer\\Desktop\\A.txt";
+	cout << "Enter path to a program to load: ";
+	char c[200];
+	cin >> c;
 	fstream fs;
 	fs.open(c);
 	FileReader fr(&fs);
-	ConsoleOutput out;
-	Machine m(&out);
-	MemortUnit* ram = m.getRam();
-	MemortUnit* cont = m.getConroller();
-	MemortUnit* reg = m.getRegistors();
-	fr.read(ram);
-	m.update();
-	while (m.isRunning())
+	ConsoleOutput co;
+	Machine machine(&co);
+	fr.read(machine.getRam());
+	bool run = true;
+	while (machine.isRunning() && run)
 	{
-		m.step();
+		int code;
+		cout << "Enter operation code: \n0: step the program\n1: print the program counter\n"<<
+			"2: print last instruction\n3: print ram\n4: print registors\n5: print machine state\n10: exit\n\n";
+		cin >> code;
+		switch (code)
+		{
+		case 0:
+			machine.step();
+			break;
+		case 1:
+			co.UpdateProgramCounter(machine.getConroller()->get(0));
+			break;
+		case 2:
+			co.UpdateLastInstruction(machine.getConroller()->readInstruction(1));
+			break;
+		case 3:
+			co.PrintTitle("Ram:\n");
+			machine.getRam()->update();
+			break;
+		case 4:
+			co.PrintTitle("Registors:\n");
+			machine.getRegistors()->update();
+			break;
+		case 5:
+			machine.update();
+			break;
+		case 10:
+			run = false;
+			break;
+		default:
+			break;
+		}
 	}
-	m.update();
+	cout << "Program finished.\n";
+	machine.update();
+	cout << "Program finished.\n";
 	return 0;
 }
