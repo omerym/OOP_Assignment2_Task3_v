52774d6e197b3563bb6a755ab8c7b72882b66f46
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2023-11-13 13:27:53 +0200

    Update Add.cpp

--- OOP_Assignment2_Task3_v/Add.cpp
+++ OOP_Assignment2_Task3_v/Add.cpp
@@ -9,8 +9,8 @@ public:
 	{
           unsigned short instruct= controller->readInstruction(1);
           unsigned char endplace= (instruct & 0x0f00)>>8;
-	  unsigned char Bit1=ram->get((instruct & 0x000f)); 
-          unsigned char Bit2=ram->get((instruct & 0x00f0)>>4);
+	  unsigned char Bit1=registors->get((instruct & 0x000f)); 
+          unsigned char Bit2=registors->get((instruct & 0x00f0)>>4);
           registors->set((unsigned char)endplace,(Bit1+Bit2));	
 	}
 };
