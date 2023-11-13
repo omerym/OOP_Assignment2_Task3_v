d5e9ab2c50b116ce420ec4add58c0aac37a7fc45
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2023-11-13 13:08:50 +0200

    Update LoadToRegister.cpp

--- OOP_Assignment2_Task3_v/LoadToRegister.cpp
+++ OOP_Assignment2_Task3_v/LoadToRegister.cpp
@@ -4,12 +4,6 @@
 #include "Ram.cpp"
 class loadToRegister: public Operator
 {
-  private:
-  MemortUnit* Current_ram; 
-  MemortUnit* Current_register;
-  MemortUnit* controller;
-  unsigned char address1;
-  int Mem;
   public:
   loadToRegister(MemortUnit* Current_ram,MemortUnit* Current_register, MemortUnit* controller): Operator(Current_ram,Current_register,controller) {};
 
@@ -18,7 +12,7 @@ class loadToRegister: public Operator
     unsigned short instruct= controller->readInstruction(1);
     unsigned char reg= (instruct & 0x0f00)>>8;
     unsigned char address=(instruct & 0x00ff);
-    Current_register->set((unsigned char)reg,address);
+    reg->set((unsigned char)reg,address);
 
   }
 }
