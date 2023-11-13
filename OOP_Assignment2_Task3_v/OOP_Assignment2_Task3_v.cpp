238805f31cccaec781cfe523e45feefe1097a7b4
Omer Yassir <omerym10@gmail.com>
2023-11-13 18:17:21 +0200

    rwgfsdg

--- OOP_Assignment2_Task3_v/OOP_Assignment2_Task3_v.cpp
+++ OOP_Assignment2_Task3_v/OOP_Assignment2_Task3_v.cpp
@@ -15,8 +15,8 @@ int main()
 	MemortUnit* cont = m.getConroller();
 	MemortUnit* reg = m.getRegistors();
 	ram->set(0,1);
-	*ram->at(0) = 2;
-	cout << (int)ram.get(0)<<endl;
+	*(ram->at(0)) = 2;
+	cout << (int)(ram->get(0))<<endl;
 	ram->writeInstruction(2, 0x1000);
 	m.step();
 	cout << m.isRunning() << endl;
