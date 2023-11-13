2620b89261d7b22d11a2873aefee95c983c4033d
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2023-11-13 13:25:49 +0200

    Update Add.cpp

--- OOP_Assignment2_Task3_v/Add.cpp
+++ OOP_Assignment2_Task3_v/Add.cpp
@@ -7,13 +7,11 @@ public:
 	AddOperation(MemortUnit* ram, MemortUnit* registor, MemortUnit* controller) : Operator(ram,registor,controller) {}
 	virtual void apply()
 	{
-          float s,t,r;
-            float value1 = registor[s];
-            float value2 = registor[t];
-            float sum  = value1+value2;
-            registor[r] = sum;
-  
-		
+          unsigned short instruct= controller->readInstruction(1);
+          unsigned char endplace= (instruct & 0x0f00)>>8;
+	  unsigned char Bit1=ram->get((instruct & 0x000f)); 
+          unsigned char Bit2=ram->get((instruct & 0x00f0)>>4);
+          registors->set((unsigned char)endplace,(Bit1+Bit2));	
 	}
 };
 #endif
