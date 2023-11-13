554fd928e1a1c191d6c5c81dc0c5db8c6339d97c
Omer Yassir <143016202+omerym@users.noreply.github.com>
2023-11-13 11:04:26 +0200

    Update StoreMemCell.cpp

--- OOP_Assignment2_Task3_v/StoreMemCell.cpp
+++ OOP_Assignment2_Task3_v/StoreMemCell.cpp
@@ -4,12 +4,7 @@
 #include "Ram.cpp"
 class StoreMemCell: public Operator
 {
-  private:
-  MemortUnit* Current_ram; 
-  MemortUnit* Current_register;
-  MemortUnit* controller;
-  unsigned char address1;
-  int Mem;
+
   public:
   StoreMemCell(MemortUnit* Current_ram,MemortUnit* Current_register, MemortUnit* controller): Operator(Current_ram,Current_register,controller) {};
 
@@ -18,8 +13,8 @@ class StoreMemCell: public Operator
     unsigned short instruct= controller->readInstruction(1);
     unsigned char reg= (instruct & 0x0f00)>>8;
     unsigned char address=(instruct & 0x00ff);
-    unsigned char BitPattern=Current_register->get((unsigned char)reg);
-    Current_ram->set((unsigned char)address,BitPattern);
+    unsigned char BitPattern=registors->get((unsigned char)reg);
+    ram->set((unsigned char)address,BitPattern);
 
   }
 }
